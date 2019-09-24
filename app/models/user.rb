ef022c3cccc13cac2fe55aa340c8d52305f45e96
ChaiUrs <mysorechaitra@gmail.com>
2019-09-24 04:35:53 +0000

    Created user spec model and added tests for user validations and authenticate with credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
+
 class User < ActiveRecord::Base
   has_secure_password
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { in: 6..20 }
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+
+    if email.nil? || password.nil?
+      return nil
+    end
+
+    user = User.find_by_email(email.downcase.strip)
+
+    # if user exists and password entered matches
+    if user && user.authenticate(password)
+      return user
+    end
+  
+  end
+
 end
